3ae00c6042a0933817304ec0da23b9538fb2ab21
Zishan <zishan3909@gmail.com>
2026-09-10 11:29:57 +0530

    Add comments for clarity on inheritance access specifiers
    Added comments to explain the purpose of the program regarding access specifiers in inheritance.

--- semester-3/OOP-with-CPP/Q39.cpp
+++ semester-3/OOP-with-CPP/Q39.cpp
@@ -1,4 +1,6 @@
-// C++ program to demonstrate the access specifiers in inheritance
+//Create a Base class that consists of private, protected and public data members and member functions.
+//Try using different access modifiers for inheriting Base class to the Derived class and create a table that summarizes the above three modes (when derived in public, protected and private modes)
+//and shows the access specifier of the members of base class in the derived class.
 #include <iostream>
 using namespace std;
 
